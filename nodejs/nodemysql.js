29c71f6f6e5d632a12d950f1a55f8c2c28526838
mansiaddweb <mansi.s.addweb@gmail.com>
2021-12-22 17:17:31 +0530

    mail and nodejs

---
+++ nodejs/nodemysql.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "org"
+});
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM student", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
---
+++ nodejs/writebuff.js
@@ -0,0 +1,3 @@
+cbuf = new Buffer(256);
+bufferlen = cbuf.write("Learn Programming with NodeJS!!!");
+console.log("No. of Octets in which string is written : "+  bufferlen);
